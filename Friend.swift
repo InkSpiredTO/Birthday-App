4f9d2efdd66e0a4df00f94412fe30782ff6529ab
Celine <futuremakersto@gmail.caom>
2025-07-13 19:12:28 -0400

    I added a navigation title and the ability to view a friend's birthday.

---
+++ Friend.swift
@@ -0,0 +1,18 @@
+//
+//  Friend.swift
+//  Birthday App
+//
+//  Created by Future Makers on 2025-07-13.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
