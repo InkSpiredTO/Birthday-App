b8cf31c916d3fc5480628a705a617498c1502b7c
Celine <futuremakersto@gmail.caom>
2025-07-13 18:22:34 -0400

    Initial Commit

---
+++ Birthday App/Birthday_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Birthday_AppApp.swift
+//  Birthday App
+//
+//  Created by Future Makers on 2025-07-13.
+//
+
+import SwiftUI
+
+@main
+struct Birthday_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Birthday AppTests/Birthday_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Birthday_AppTests.swift
+//  Birthday AppTests
+//
+//  Created by Future Makers on 2025-07-13.
+//
+
+import Testing
+
+struct Birthday_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
