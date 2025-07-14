1f14840a60da0e04145303e3380cba92ed320abf
Celine <futuremakersto@gmail.caom>
2025-07-13 20:05:51 -0400

    You can now add friend's birthdays to the app.

--- Birthday App/ContentView.swift
+++ Birthday App/ContentView.swift
@@ -10,9 +10,11 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var friends: [Friend] = [
-        Friend(name: "Elton Lin", birthday: .now),
-        Friend(name: "Jenny Court", birthday: Date(timeIntervalSince1970: 0))
     ]
+    
+    @State private var newName = ""
+    
+    @State private var newBirthday = Date.now
     
     var body: some View {
         NavigationStack{
@@ -23,7 +25,35 @@ struct ContentView: View {
                     Text(friend.birthday, format: .dateTime.month(.wide).day().year())
                 }
             }
+            
             .navigationTitle("Birthdays")
+            
+            .safeAreaInset(edge: .bottom){
+                
+                VStack(alignment: .center, spacing: 20){
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                                TextField("Name", text: $newName)
+                                    .textFieldStyle(.roundedBorder)
+                    }
+                    
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+
+                    }
+                    
+                    .padding(.horizontal)
+                    
+                    
+                    
+                }
+            }
+            
+            
         }
     }
 }
